833fc96f78cfc8d502cc865f0969db642060e0a6
zuenamash <mnachariazuenah@gmail.com>
2022-03-10 09:48:13 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var college = "codeHive"
+    var result=divide(a:35:b:23)
+    println(result)
+    fact(w:"zuenah",c:21)
+    python(good"i can do it")
+}
+fun college(name:String){
+    var text1= name[5]
+    var text2= name[6]
+    var text3= name[7]
+    var text4= name[8]
+    println(text1.toString()+text2+text3+ text4)
+
+
+}
+fun divide(a:'int'b:'int'):'int'{
+    var modulous=a%b
+    return modulous
+
+}
+fun fact(w:String,c:int):String{
+    var come="hi,my name is $w and i am $c years old"
+    println(come)
+    return come
+}
+fun python(good:String):String{
+    println(good:length)
+    return good
+}
